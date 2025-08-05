4ec3215d14fc8c505c914e60573cbee9486f8dea
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2025-08-04 22:37:51 -0400

    Add number subtraction

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+
 def sub(a: int, b: int) -> int:
     return a - b
 
@@ -8,3 +9,4 @@ if __name__ == "__main__":
 
     print(f"2 + 3 = {add(2, 3)}")
     print(f"2 - 3 = {sub(2, 3)}")
+    print(f"5 - 3 = {sub(5, 3)}")
