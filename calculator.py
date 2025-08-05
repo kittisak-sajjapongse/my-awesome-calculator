ef05e54de0bd9b6d6c9539e4bf900d40090c79c3
Kittisak Sajjapongse <63345597+kittisak-sajjapongse@users.noreply.github.com>
2025-08-05 17:58:27 -0400

    Implemented mul (#2)

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 
-sdfds
+def mul(a: int, b: int) -> int:
+    return a * b
 
 def div(a: int, b: int) -> int:
     return a / b
@@ -15,3 +16,4 @@ if __name__ == "__main__":
     print(f"2 + 3 = {add(2, 3)}")
     print(f"2 - 3 = {sub(2, 3)}")
     print(f"5 - 3 = {sub(5, 3)}")
+    print(f"10 * 4 = {mul(10, 4)}")
