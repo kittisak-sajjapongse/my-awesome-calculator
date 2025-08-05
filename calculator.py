6296fe85a5d615351d4233d8e8a3c46a06af55c3
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2025-08-04 22:32:58 -0400

    Implemented subtraction

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
+def sub(a: int, b: int) -> int:
+    return a - b
+
 def add(a: int, b: int) -> int:
     return a + b
 
 if __name__ == "__main__":
 
     print(f"2 + 3 = {add(2, 3)}")
+    print(f"2 - 3 = {sub(2, 3)}")
