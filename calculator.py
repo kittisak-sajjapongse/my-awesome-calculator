64a426d2a740255f7b09afdf6817f9da81fb6c62
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2025-08-05 23:10:51 -0400

    Added rectangle area

--- calculator.py
+++ calculator.py
@@ -11,6 +11,9 @@ def sub(a: int, b: int) -> int:
 def add(a: int, b: int) -> int:
     return a + b
 
+def triangle_area(width, height):
+    return 0.5 * width * height
+
 if __name__ == "__main__":
 
     print(f"2 + 3 = {add(2, 3)}")
@@ -18,3 +21,4 @@ if __name__ == "__main__":
     print(f"5 - 3 = {sub(5, 3)}")
     print(f"7 - 3 = {sub(7, 3)}")
     print(f"10 * 4 = {mul(10, 4)}")
+    print(f"triangle {triangle_area(3,5)}")
